83de9975a494e6f4ef337491491e81b32474d68a
lucaskbr <lucas_klasa@hotmail.com>
2020-04-17 19:19:40 -0300

    feat: allow to like a repository

--- src/app.js
+++ src/app.js
@@ -68,7 +68,18 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json("The repository doesnt exists");
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
