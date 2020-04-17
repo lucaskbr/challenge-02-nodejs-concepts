f8c9d2eb5c74e7438139e4be2f24a2c4ea5654d2
lucaskbr <lucas_klasa@hotmail.com>
2020-04-17 19:04:19 -0300

    feat: allow to delete a repository

--- src/app.js
+++ src/app.js
@@ -53,7 +53,18 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json("The repository doesnt exists");
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).json();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
